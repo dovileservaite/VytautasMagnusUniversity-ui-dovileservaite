21551af16171c742672a2706b001836d7deaf3df
User1 <User1@DESKTOP-DS1E0BG>
2017-04-26 11:32:15 +0300

    GVS

---
+++ ld5/ld5.cpp
@@ -0,0 +1,11 @@
+#include "ld5.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]//leave this as is
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew ld5:: ld5);
+}
